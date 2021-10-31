3c0c2ed4417037e52db4f17e7c191de8ea307655
Mohit Pandey <50360364+ecomohitpande@users.noreply.github.com>
2021-10-31 22:39:34 +0100

    Update ui.R

--- Forecasting App/ui.R
+++ Forecasting App/ui.R
@@ -1,40 +1,40 @@
 library(shiny)
-library(data.table)
-library(randomForest)
+
 # Training set
 TrainSet <- read.csv("training.csv", header = TRUE)
 TrainSet <- TrainSet[,-1]
 
- pageWithSidebar(
-  
+pageWithSidebar(
+
   # Page header
-  headerPanel('Credit Default'),
-  
+  headerPanel('Iris Predictor'),
+
   # Input values
   sidebarPanel(
-    #HTML("<h3>Input parameters</h3>"),
-    tags$label(h3('Input parameters')),
-    numericInput("Education", 
-                 label = "Education", 
-                 value = 1),
-    numericInput("Marriage", 
-                 label = "Marriage", 
-                 value = 2),
-    numericInput("Age", 
-                 label = "Age", 
-                 value = 24),
-    numericInput("BILL", 
-                 label = "BILL", 
-                 value = 3913),
-    
-    actionButton("submitbutton", "Submit", 
-                 class = "btn btn-primary")
+    HTML("<h3>Input parameters</h4>"),
+    sliderInput("Sepal.Length", label = "Sepal Length", value = 5.0,
+                min = min(TrainSet$Sepal.Length),
+                max = max(TrainSet$Sepal.Length)
+    ),
+    sliderInput("Sepal.Width", label = "Sepal Width", value = 3.6,
+                min = min(TrainSet$Sepal.Width),
+                max = max(TrainSet$Sepal.Width)),
+    sliderInput("Petal.Length", label = "Petal Length", value = 1.4,
+                min = min(TrainSet$Petal.Length),
+                max = max(TrainSet$Petal.Length)),
+    sliderInput("Petal.Width", label = "Petal Width", value = 0.2,
+                min = min(TrainSet$Petal.Width),
+                max = max(TrainSet$Petal.Width)),
+
+    actionButton("submitbutton", "Submit", class = "btn btn-primary")
   ),
-  
+
   mainPanel(
     tags$label(h3('Status/Output')), # Status/Output Text Box
     verbatimTextOutput('contents'),
     tableOutput('tabledata') # Prediction results table
-    
+
   )
 )
+
+
