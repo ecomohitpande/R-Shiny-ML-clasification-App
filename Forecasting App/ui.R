7c968e3137173522e1f5353b9d8129eaa3f386ab
Mohit Pandey <50360364+ecomohitpande@users.noreply.github.com>
2021-11-01 17:19:19 +0100

    Update ui.R

--- Forecasting App/ui.R
+++ Forecasting App/ui.R
@@ -4,38 +4,36 @@ library(shiny)
 TrainSet <- read.csv("training.csv", header = TRUE)
 TrainSet <- TrainSet[,-1]
 
-pageWithSidebar(
-
+ pageWithSidebar(
+  
   # Page header
   headerPanel('Iris Predictor'),
-
+  
   # Input values
   sidebarPanel(
-    HTML("<h3>Input parameters</h4>"),
-    sliderInput("Sepal.Length", label = "Sepal Length", value = 5.0,
-                min = min(TrainSet$Sepal.Length),
-                max = max(TrainSet$Sepal.Length)
-    ),
-    sliderInput("Sepal.Width", label = "Sepal Width", value = 3.6,
-                min = min(TrainSet$Sepal.Width),
-                max = max(TrainSet$Sepal.Width)),
-    sliderInput("Petal.Length", label = "Petal Length", value = 1.4,
-                min = min(TrainSet$Petal.Length),
-                max = max(TrainSet$Petal.Length)),
-    sliderInput("Petal.Width", label = "Petal Width", value = 0.2,
-                min = min(TrainSet$Petal.Width),
-                max = max(TrainSet$Petal.Width)),
-
-    actionButton("submitbutton", "Submit", class = "btn btn-primary")
+    #HTML("<h3>Input parameters</h3>"),
+    tags$label(h3('Input parameters')),
+    numericInput("Sepal.Length", 
+                 label = "Sepal Length", 
+                 value = 5.1),
+    numericInput("Sepal.Width", 
+                 label = "Sepal Width", 
+                 value = 3.6),
+    numericInput("Petal.Length", 
+                 label = "Petal Length", 
+                 value = 1.4),
+    numericInput("Petal.Width", 
+                 label = "Petal Width", 
+                 value = 0.2),
+    
+    actionButton("submitbutton", "Submit", 
+                 class = "btn btn-primary")
   ),
-
+  
   mainPanel(
     tags$label(h3('Status/Output')), # Status/Output Text Box
     verbatimTextOutput('contents'),
     tableOutput('tabledata') # Prediction results table
-
+    
   )
 )
-
-
-
