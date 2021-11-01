5642b7b62623bb059762b797f1bc2b3669695b50
Mohit Pandey <50360364+ecomohitpande@users.noreply.github.com>
2021-11-01 22:28:51 +0100

    Update server.R

--- Forecasting App/server.R
+++ Forecasting App/server.R
@@ -11,9 +11,9 @@ shinyServer(function(input, output, session) {
     
     df <- data.frame(
       Name = c("Education",
-               "Marital.Status",
+               "Marriage",
                "Age",
-               "BILL.Amount"),
+               "BILL"),
       Value = as.character(c(input$Education,
                              input$Marriage,
                              input$Age,
